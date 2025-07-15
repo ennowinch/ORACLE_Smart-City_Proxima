acc1158f974becdb8c1dea6156a9bec5eacbf1d4
ljiscoding <jankeluise33@gmail.com>
2025-07-15 12:42:01 +0200

    Add files via upload

---
+++ temperature.ino
@@ -0,0 +1,72 @@
+#include "DHT.h"
+#include <WiFi101.h>
+
+#define DHTPIN 2      // Pin, an dem der DHT11 angeschlossen ist
+#define DHTTYPE DHT11 // DHT 11 Sensor
+const char* ssid = "INDU4GY";
+const char* password = "indu8764";
+char server[] = "192.168.1.200";
+bool einmalausfuehren = true;
+
+WiFiClient client;
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  
+  dht.begin();
+  Serial.println("DHT11 Sensor starten...");
+
+  WiFi.begin(ssid, password);
+  Serial.print("Verbinde mit WLAN...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nVerbunden!");
+}
+
+void loop() {
+  delay(2000); // 2 Sekunden zwischen den Messungen
+
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Fehler beim Lesen vom Sensor!");
+    return;
+  }
+
+  Serial.print("Luftfeuchtigkeit: ");
+  Serial.print(humidity);
+  Serial.print(" %\t");
+  Serial.print("Temperatur: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  if(humidity >= 80){
+    Serial.println("Attention: very high humdidity!");
+  }
+
+  if (client.connect(server, 80)) {
+    if (einmalausfuehren == true){
+      Serial.println("Mit Server verbunden");
+      einmalausfuehren = false;
+    }
+    client.print("Luftfeuchtigkeit: ");
+    client.print(humidity);
+    client.print(" %\t");
+    client.print("Temperatur: ");
+    client.print(temperature);
+    client.println(" °C");
+    String antwort = client.readStringUntil('\n');
+    client.stop();
+
+  } else {
+    Serial.println("Verbindung fehlgeschlagen");
+  }
+
+  delay(2000); // alle 5 Sekunden neu senden
+}
