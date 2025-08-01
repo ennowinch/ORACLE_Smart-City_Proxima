1856957503357a957a3108e6ddf2e2b2cb54f698
enno_winch <EnnoWinch@web.de>
2025-08-01 23:51:04 +0200

    ORACLE SmartCity-Proxima (1.0) - Pre-release /
    V1.0 - Fixing Bugs, Clearer Language --> Only English

--- Windanlage_Master.ino
+++ Windanlage_Master.ino
@@ -3,7 +3,7 @@ Copyright (c) 2025 Enno Winch
 // This file is part of the [Oracle SmartCity - Proxima] project and is licensed under the MIT License.
 // See the "LICENSE" file in the root directory for details.
 
-// the following "/[...]" or "(/[...]) will be used to explain more in depth or to lead to other explicit resources.
+// the following "/[...]" or "(/[...])" will be used to explain more in depth or to lead to other explicit resources.
 
 #include <WiFi101.h>
 WiFiServer server(80);
@@ -14,8 +14,8 @@ bool einmalausfuehren = true;
 int relaisPin = 5; // !Digitaler! GPIO-PIN für Relay (in diesem Fall "5" /siehe Dokumentation: "Wiring-RX")
 float humidity = 0.0;
 float temperature = 0.0;
-char ssid[] = "INDU4GY"; // Dein Netzwerk Name/SSID
-char pass[] = "indu8764"; // Dein Password 
+char ssid[] = "INDU4GY"; // Your Network Name/SSID
+char pass[] = "indu8764"; // Your Password
 
 IPAddress ip(192, 168, 1, 200);     // Server-IP (Sensor)
 IPAddress gateway(192, 168, 1, 1);  // Router-IP
@@ -27,7 +27,7 @@ void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  WiFi.config(ip, gateway, subnet); // Setzen fester IP 
+  WiFi.config(ip, gateway, subnet); // Set static IP
   WiFi.begin(ssid, pass);
 
   Serial.print("Verbinde mit WLAN...");
@@ -36,7 +36,7 @@ void setup() {
     Serial.print(".");
   }
 
-  Serial.println("\nVerbunden!"); // Verbindung zwischen Arduino & Netzwerk erfolgreich!
+  Serial.println("\nVerbunden!"); // Connection between Arduino & network successful!
   Serial.print("IP-Adresse (Server): ");
   Serial.println(WiFi.localIP());
   server.begin();
@@ -47,42 +47,41 @@ void loop() {
   WiFiClient client = server.available();
   if (client) {
     if (einmalausfuehren == true){
-      Serial.println("Client verbunden!"); // Verbinden mit Sensor- (/RX-) Arduino erfolgreich!
+      Serial.println("Client verbunden!"); // Connection to sensor (/RX) Arduino successful!
       einmalausfuehren = false;
     }
     
 
-  String empfangen = client.readStringUntil('\n');  // Empfängt die ganze Zeile
-  empfangen.trim();  // Entfernt Leerzeichen und Zeilenumbrüche
-
+  String empfangen = client.readStringUntil('\n'); // Receives the entire line
+  empfangen.trim();  // Removes spaces and line breaks
   Serial.print("Empfangen: '");
   Serial.print(empfangen);
   Serial.println("'");
 
-  // Schlüsselwörter finden
+  // Find keywords
   int lfPos = empfangen.indexOf("Luftfeuchtigkeit:");
   int tPos  = empfangen.indexOf("Temperatur:");
 
   if (lfPos != -1 && tPos != -1) {
-    // Luftfeuchtigkeit extrahieren
+    // Extract humidity
     int lfStart = lfPos + String("Luftfeuchtigkeit:").length();
     int lfEnd   = empfangen.indexOf(',', lfStart);
     String lfStr = empfangen.substring(lfStart, lfEnd);
     lfStr.trim();
 
-    // Temperatur extrahieren
+    // Extract temperature
     int tStart = tPos + String("Temperatur:").length();
     String tStr = empfangen.substring(tStart);
     tStr.trim();
 
-    // In float umwandeln – mit englischen Variablennamen
+    // Convert to float – with English variable names
     humidity = lfStr.toFloat();
     temperature = tStr.toFloat();
 
-    Serial.print("Luftfeuchtigkeit: "); // Anzeigen der <imp.> (/importierten) Luftfeuchtigkeit-Werten von TX-Arduino
+    Serial.print("Luftfeuchtigkeit: "); // Display the <imp.> (/imported) humidity values from TX-Arduino
     Serial.println(humidity);
 
-    Serial.print("Temperatur: "); // Anzeigen der <imp.> (/importierten) Temperatur-Werten von TX-Arduino
+    Serial.print("Temperatur: "); // Display the <imp.> (/imported) temperature values from TX-Arduino
     Serial.println(temperature);
   } else {
     Serial.println("Fehler: 'Luftfeuchtigkeit' oder 'Temperatur' nicht gefunden!");
@@ -97,15 +96,15 @@ void loop() {
     client.stop();
     
 
-    if(humidity >= 80){ // - (Relative Feuchtigkeit in Prozent (RFP) /) Schwellenwert überschritten/gleich [>/=] -
-      anaus = false; // - An/Aus Relais
+    if(humidity >= 80){ // - (Relative humidity in percent (RHP) /) Threshold value exceeded/equal to [>/=] -
+      anaus = false; // - On/Off relay
     }
     else{
       anaus = true;
     }
 
     if(anaus == true) {
-      digitalWrite(relaisPin, HIGH); // - Relais: ON / OFF - [bei TRUE bzw. FAlSE] (siehe bei: "else if")
+      digitalWrite(relaisPin, HIGH); // - Relay: ON / OFF - [if TRUE or FALSE] (see: “else if”)
     }
     else if(anaus == false) {
       digitalWrite(relaisPin, LOW);
@@ -117,4 +116,4 @@ void loop() {
   }
     
 } 
- // The code was developed by Luise J. & Enno W. during their student internship at Oracle. All rights reserved --> see "LICENSE"
+ // The code was developed by Luise J. & Enno W. during their student internship at Oracle. All rights reserved --> see "LICENSE" for details.
